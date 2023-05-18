e2b22a3681627f1047a075c548e42f11ffb8b142
Pylos7 <nestor1ingles@gmail.com>
2023-05-18 18:34:22 -0400

    Added favicon

--- server.py
+++ server.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 print(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello!!</p>"
+    # Render_template looks for files in the templates folder
+    return render_template("/index.html")
+
+@app.route("/about.html")
+def about():
+    return render_template("/about.html")
 
 @app.route("/blog")
 def blog():
-    return "<pThese are my thoughts on blogs</p>"
+    return "<p>These are my thoughts on blogs</p>"
+
+@app.route("/blog/2020/dogs")
+def blog2():
+    return "<p>This is my dog!</p>"
