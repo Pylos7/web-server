06182b4fe9d178cf5675a45a1cb446c7fcdb4ac3
Pylos7 <nestor1ingles@gmail.com>
2023-05-19 10:32:20 -0400

    Sample template working

--- server.py
+++ server.py
@@ -3,20 +3,10 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 print(__name__)
 
-@app.route("/<username>/<int:post_id>")
-def hello_world(username=None, post_id=None):
-    print(url_for('static', filename='favicon.ico'))
-    # Render_template looks for files in the templates folder
-    return render_template("/index.html", name=username, post_id=post_id)
+@app.route("/")
+def my_home():
+    return render_template("/index.html")
 
-@app.route("/about.html")
-def about():
-    return render_template("/about.html")
-
-@app.route("/blog")
-def blog():
-    return "<p>These are my thoughts on blogs</p>"
-
-@app.route("/blog/2020/dogs")
-def blog2():
-    return "<p>This is my dog!</p>"
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    return render_template(page_name)
