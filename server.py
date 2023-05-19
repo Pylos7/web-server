f97c97587416aa125a01e37b27722b81661d5d97
Pylos7 <nestor1ingles@gmail.com>
2023-05-18 20:03:21 -0400

    Getting variables from web address

--- server.py
+++ server.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 print(__name__)
 
-@app.route("/")
-def hello_world():
+@app.route("/<username>/<int:post_id>")
+def hello_world(username=None, post_id=None):
+    print(url_for('static', filename='favicon.ico'))
     # Render_template looks for files in the templates folder
-    return render_template("/index.html")
+    return render_template("/index.html", name=username, post_id=post_id)
 
 @app.route("/about.html")
 def about():
